0a703941afec822b41a26caf975b3a8da70be917
vleon <vleon@socaportuguesa.com>
2017-02-02 10:17:02 -0400

    uri ssl2

--- Servidor/Core/Core.js
+++ Servidor/Core/Core.js
@@ -1,5 +1,5 @@
  var pg = require('pg');
-connectionString = process.env.DATABASE_URL+'?ssl=true' || 'postgres://postgres:1234@localhost:5432/frasesweb';
+connectionString = 'postgres=//cptscthbaftblv=f408a51f0b638c21f402ae9b24650a1d0c4f0d39b2258da9ba374e78525d0741@ec2-54-221-201-244.compute-1.amazonaws.com=5432/d9odifbf15mthd?ssl=true?ssl=true' || 'postgres://postgres:1234@localhost:5432/frasesweb';
 console.log(connectionString);
 var client = new pg.Client(connectionString);
 client.connect();
