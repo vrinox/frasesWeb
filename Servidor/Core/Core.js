336713fc02972586a70f6add1648e28bc643c8e9
vleon <vleon@socaportuguesa.com>
2017-02-02 08:02:26 -0400

    prueba databaseURL

--- Servidor/Core/Core.js
+++ Servidor/Core/Core.js
@@ -1,5 +1,6 @@
  var pg = require('pg');
 connectionString = process.env.DATABASE_URL || 'postgres://postgres:1234@localhost:5432/frasesWeb';
+console.log(connectionString);
 var client = new pg.Client(connectionString);
 client.connect();
 
