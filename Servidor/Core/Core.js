7ec453514f9cb6f01e7029f8a54b6dbf373e783c
vleon <vleon@socaportuguesa.com>
2017-02-02 11:08:24 -0400

    uri ssl5

--- Servidor/Core/Core.js
+++ Servidor/Core/Core.js
@@ -1,4 +1,5 @@
- var pg = require('pg').native;
+ var pg = require('pg');
+pg.defaults.ssl = true;
 connectionString = process.env.DATABASE_URL+'?sslmode=require' || 'postgres://postgres:1234@localhost:5432/frasesweb';
 console.log(connectionString);
 var client = new pg.Client(connectionString);
