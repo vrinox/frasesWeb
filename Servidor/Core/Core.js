4320a1e8c91f683c37139517af27cb0921e85236
vleon <vleon@socaportuguesa.com>
2017-02-02 09:18:37 -0400

    prueba hosting

--- Servidor/Core/Core.js
+++ Servidor/Core/Core.js
@@ -1,5 +1,5 @@
  var pg = require('pg');
-connectionString = process.env.DATABASE_URL || 'postgres://postgres:1234@localhost:5432/frasesWeb';
+connectionString = /*process.env.DATABASE_URL ||*/ 'postgres://cptscthbaftblv:f408a51f0b638c21f402ae9b24650a1d0c4f0d39b2258da9ba374e78525d0741@ec2-54-221-201-244.compute-1.amazonaws.com:5432/d9odifbf15mthd';
 console.log(connectionString);
 var client = new pg.Client(connectionString);
 client.connect();
