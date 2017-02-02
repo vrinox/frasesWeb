f2d75d3632cbdcf2fe5c9ae7b1f7b8cdc893fcc9
vleon <vleon@socaportuguesa.com>
2017-02-02 10:00:43 -0400

    parametros por uri3

--- Servidor/Core/Core.js
+++ Servidor/Core/Core.js
@@ -4,7 +4,7 @@ connectionParams={
   user: process.env.DATABASE_USER || 'postgres',
   password: process.env.DATABASE_PASS || '1234',
   database: process.env.DATABASE || 'frasesweb',
-  port: proces.env.DATABASE_PORT || '5432',
+  port: process.env.DATABASE_PORT || '5432',
   ssl: true
 };
 console.log(connectionParams);
